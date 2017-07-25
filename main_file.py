2c0349a5dd12c5df34a8059f3fba9c78d0d07bdc
roi3579 <roi3579@gmail.com>
2017-07-25 16:49:52 +0300

    run features degree,pagerank,kcore,closeness,motif_3,motif_4 on sample from file.

--- main_file.py
+++ main_file.py
@@ -1,44 +1,63 @@
 from utils.graph_wrapper import GraphWrapper
 from utils.features_saver import FeatureSaver
-import datetime
-import random
+from datetime import datetime
 import sys
 
+def write_log(s):
+    print s, datetime.now()
+
+i = int(sys.argv[1])
+if i<10:
+    snap = '000' +str(i)
+else:
+    snap = '00' + str(i)
+
+# sample_size = 500000
+sample_size = int(sys.argv[2])
+
+output_dir = './data/directed/livejournal/snap{0}/'.format(snap)
 graph_wrapper = GraphWrapper()
 feature_saver = FeatureSaver()
 
-# graph_wrapper.load_from_file(is_directed=True,file_path='./data/roi_data/input.txt')
-graph_wrapper.load_from_file(is_directed=True,file_path='./data/roi_data/cora.txt')
-# graph_wrapper.print_vertices_list()
-# graph_wrapper.print_edges_list()
+graph_wrapper.load_from_file(is_directed=True, file_path='./data/directed/livejournal/snap0001/uniform_sample_p_{0}.txt'.format(sample_size))
+print 'edges:',len(graph_wrapper.get_edges_list())
+print 'vertices:',len(graph_wrapper.get_vertices_list())
 
-kcore_features= graph_wrapper.k_coreness()
-# print kcore_features
-feature_saver.save_vertex_features_to_file(kcore_features, './data/roi_data/kcore.txt')
-
-pagerank_features= graph_wrapper.page_rank()
-# print pagerank_features
-feature_saver.save_vertex_features_to_file(pagerank_features, './data/roi_data/page_rank.txt')
-
-closeness_features= graph_wrapper.closeness()
-# print closeness_features
-feature_saver.save_vertex_features_to_file(closeness_features, './data/roi_data/closeness.txt')
-
-betweenness_features= graph_wrapper.betweenness()
-# print betweenness_features
-feature_saver.save_vertex_features_to_file(betweenness_features, './data/roi_data/betweenness.txt')
-
-bfs_moments= graph_wrapper.bfs_moments()
-# print bfs_moments
-feature_saver.save_vertex_features_to_file(bfs_moments, './data/roi_data/bfs_moments.txt')
-
+write_log('start degree')
 degrees = graph_wrapper.degree()
-# print bfs_moments
-feature_saver.save_vertex_features_to_file(degrees, './data/roi_data/degrees.txt')
+feature_saver.save_vertex_features_to_file(degrees, output_dir+'/degrees_{0}.txt'.format(sample_size))
 
+write_log('start k-core')
+kcore_features= graph_wrapper.k_coreness()
+feature_saver.save_vertex_features_to_file(kcore_features, output_dir+'/kcore_{0}.txt'.format(sample_size))
+
+write_log('start page-rank')
+pagerank_features= graph_wrapper.page_rank()
+feature_saver.save_vertex_features_to_file(pagerank_features, output_dir+'/page_rank_{0}.txt'.format(sample_size))
+
+write_log('start closeness')
+closeness_features= graph_wrapper.closeness()
+feature_saver.save_vertex_features_to_file(closeness_features, output_dir+'/closeness_{0}.txt'.format(sample_size))
+
+# write_log('start betweenness')
+# betweenness_features= graph_wrapper.betweenness()
+# feature_saver.save_vertex_features_to_file(betweenness_features, output_dir+'/betweenness.txt')
+
+write_log('start bfs')
+bfs_moments= graph_wrapper.bfs_moments()
+feature_saver.save_vertex_features_to_file(bfs_moments, output_dir+'/bfs_moments_{0}.txt'.format(sample_size))
+
+write_log('start motif 3')
 motifs = graph_wrapper.motif(vertices_list=None, motif_veriation_folder='./graph_algos/motifs_veriation',motif_size=3)
-feature_saver.save_vertex_features_to_file(motifs, './data/roi_data/motifs_3.txt')
+feature_saver.save_vertex_features_to_file(motifs, output_dir+'/motifs_3_{0}.txt'.format(sample_size))
 
+write_log('start motif 4')
 motifs = graph_wrapper.motif(vertices_list=None, motif_veriation_folder='./graph_algos/motifs_veriation',motif_size=4)
-feature_saver.save_vertex_features_to_file(motifs, './data/roi_data/motifs_4.txt')
+feature_saver.save_vertex_features_to_file(motifs, output_dir+'/motifs_4_{0}.txt'.format(sample_size))
+
+# print 'start flow',datetime.datetime.now()
+# flows = graph_wrapper.flow()
+# feature_saver.save_vertex_features_to_file(flows, output_dir+'/flow.txt')
+# print 'finish',datetime.datetime.now()
+
 
