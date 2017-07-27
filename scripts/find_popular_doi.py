9cbe41b9a4a7162cb5f0bd20deba2b06bb141ce4
roi3579 <roi3579@gmail.com>
2017-07-27 13:27:08 +0300

    find popular_doi script

---
+++ scripts/find_popular_doi.py
@@ -0,0 +1,34 @@
+import json
+import pymysql
+import operator
+
+file_path = r'./../data/directed/livejournal/snap0001/input.json'
+with open(file_path, 'r') as f:
+    db_paras = json.load(f)
+connection_params = db_paras['connection_details']
+cnx = pymysql.connect(user=connection_params['user'],
+                      password=connection_params['pass'],
+                      host=connection_params['host'],
+                      database=connection_params['database'])
+
+cursor = cnx.cursor()
+
+vertices = set([w.split()[0] for w in open('./../data/directed/livejournal/snap0001/uniform_sample_p_500000.txt')])
+# print vertices[0]
+print len(vertices)
+
+query = 'SELECT InterestId FROM ljhistory.userinterests_0001'
+cursor.execute(query)
+doi_count = {}
+for doi in cursor:
+    if doi in doi_count:
+        doi_count[doi] += 1
+    else:
+        doi_count[doi] = 1
+
+# print doi_count
+sorted_doi = sorted(doi_count.items(), key=operator.itemgetter(1),reverse=True)
+with open('./../data/directed/livejournal/popular_doi.txt','w') as f:
+    for doi in sorted_doi[0:2000]:
+        f.writelines('{0},{1}\n'.format(doi[0][0],doi[1]))
+print 'end'
