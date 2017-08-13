1213b4890076754ae1e67124e871c4ab97f25eb7
roi3579 <roi3579@gmail.com>
2017-08-13 18:18:58 +0300

    adding motif4

--- learning/learning_main.py
+++ learning/learning_main.py
@@ -11,7 +11,8 @@ def get_learning_loader(dir_path):
                                     'page_rank_750000.txt',
                                     'closeness_750000.txt',
                                     'bfs_moments_750000.txt',
-                                    'motifs_3_750000.txt')
+                                    'motifs_3_750000.txt',
+                                    'motifs_4_750000.txt')
     leaning_loader.load_tags_from_file(dir_path+'/doi_750000/music.txt')
 
     return leaning_loader
@@ -26,7 +27,7 @@ y = learning_object.tags_vector
 print x.shape
 print y.shape
 deep = bool(int(sys.argv[1]))
-# deep = Fals
+# deep = False
 if(deep):
     from deep_learning import DeepLearning
     learning = DeepLearning(learning_object)
@@ -35,14 +36,25 @@ if(deep):
 else:
     learning = ClassicMachineLearning(learning_object)
     algo_name = sys.argv[2]
-    # algo_name = 'adaBoost'
+    # algo_name = 'RF'
     print algo_name
-    clf = learning.run_learning(test_size=0.0, algo_name=algo_name)
+    clf = learning.run_learning(test_size=0.2, algo_name=algo_name)
 
 print 'auc train:',learning.evaluate_AUC_train()
 print 'auc test:',learning.evaluate_AUC_test()
+# print 'auc test:',learning.evaluate_AUC_test()
 
-# dir_path = r'./../data/directed/livejournal/snap0002/'
-# learning_loader2 = get_learning_loader(dir_path)
-# learning_object2 = learning_loader2.get_learning_object(zscoring=True)
-# print 'auc snap0002:',learning.evaluate_AUC_general(clf, learning_object2.features_matrix, learning_object2.tags_vector)
+dir_path = r'./../data/directed/livejournal/snap0002/'
+learning_loader2 = get_learning_loader(dir_path)
+learning_object2 = learning_loader2.get_learning_object(zscoring=True)
+print 'auc snap 0002:',learning.evaluate_AUC_general(clf, learning_object2.features_matrix, learning_object2.tags_vector)
+
+dir_path = r'./../data/directed/livejournal/snap0010/'
+learning_loader3 = get_learning_loader(dir_path)
+learning_object3 = learning_loader3.get_learning_object(zscoring=True)
+print 'auc snap 0010:',learning.evaluate_AUC_general(clf, learning_object3.features_matrix, learning_object3.tags_vector)
+
+dir_path = r'./../data/directed/livejournal/snap0019/'
+learning_loader4 = get_learning_loader(dir_path)
+learning_object4 = learning_loader4.get_learning_object(zscoring=True)
+print 'auc snap0019:',learning.evaluate_AUC_general(clf, learning_object4.features_matrix, learning_object4.tags_vector)
